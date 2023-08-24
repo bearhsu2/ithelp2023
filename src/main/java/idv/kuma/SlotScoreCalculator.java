f6abe1be697525a134df031cf9d4f2c87ac49c01
Yu-Song Syu <kuma@cloud-latitude.com>
2023-08-24 22:59:31 +0800

    ok

---
+++ src/main/java/idv/kuma/SlotScoreCalculator.java
@@ -0,0 +1,4 @@
+package idv.kuma;
+
+public class SlotScoreCalculator {
+}
---
+++ src/test/java/idv/kuma/SlotScoreCalculatorTest.java
@@ -0,0 +1,11 @@
+package idv.kuma;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SlotScoreCalculatorTest {
+    @Test
+    void name() {
+        Assertions.assertThat(1).isEqualTo(1);
+    }
+}
