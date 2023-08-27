c675f790adb940b418aa3887f20b7108b625d884
Yu-Song Syu <kuma@cloud-latitude.com>
2023-08-27 23:53:21 +0800

    one line

--- src/test/java/idv/kuma/SlotScoreCalculatorTest.java
+++ src/test/java/idv/kuma/SlotScoreCalculatorTest.java
@@ -3,13 +3,41 @@ package idv.kuma;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class SlotScoreCalculatorTest {
+    @Test
+    void one_line() {
+
+        SlotScoreCalculator sut = new SlotScoreCalculator(
+                List.of(
+                        List.of("A", "1", "2"),
+                        List.of("A", "1", "2"),
+                        List.of("A", "1", "2"),
+                        List.of("A", "1", "2"),
+                        List.of("A", "2", "3")
+                )
+        );
+
+        int win = sut.calculate(10);
+
+        Assertions.assertThat(win).isEqualTo(400);
+    }
+
     @Test
     void lose() {
 
-        SlotScoreCalculator sut = new SlotScoreCalculator();
+        SlotScoreCalculator sut = new SlotScoreCalculator(
+                List.of(
+                        List.of("A", "1", "2"),
+                        List.of("A", "1", "2"),
+                        List.of("A", "1", "2"),
+                        List.of("A", "1", "2"),
+                        List.of("1", "2", "3")
+                )
+        );
 
-        int win = sut.calculate();
+        int win = sut.calculate(10);
 
         Assertions.assertThat(win).isEqualTo(0);
     }
