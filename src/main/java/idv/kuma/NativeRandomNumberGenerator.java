449f105f1fbe16948931d044614f7ba12536fb7f
Yu-Song Syu <kuma@cloud-latitude.com>
2023-09-06 00:00:55 +0800

    extract interface

---
+++ src/main/java/idv/kuma/NativeRandomNumberGenerator.java
@@ -0,0 +1,17 @@
+package idv.kuma;
+
+import java.util.Random;
+
+public class NativeRandomNumberGenerator implements RandomNumberGenerator {
+    private final Random random;
+
+    public NativeRandomNumberGenerator(Random random) {
+        this.random = random;
+    }
+
+    @Override
+    public int nextInt(int bound) {
+        return random.nextInt(bound);
+    }
+
+}
--- src/main/java/idv/kuma/RandomNumberGenerator.java
+++ src/main/java/idv/kuma/RandomNumberGenerator.java
@@ -1,19 +1,5 @@
 package idv.kuma;
 
-import java.util.Random;
-
-public class RandomNumberGenerator {
-    private final Random random;
-
-    public RandomNumberGenerator(Random random) {
-        this.random = random;
-    }
-
-    public int nextInt(int bound) {
-        return random.nextInt(bound);
-    }
-
-    private Random random() {
-        return random;
-    }
+public interface RandomNumberGenerator {
+    int nextInt(int bound);
 }
