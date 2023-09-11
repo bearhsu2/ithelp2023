170535ab26b984504293445c0ee26b67789a2839
Yu-Song Syu <kuma@cloud-latitude.com>
2023-09-11 22:20:54 +0800

    remove commented code

--- src/main/java/idv/kuma/Reels.java
+++ src/main/java/idv/kuma/Reels.java
@@ -7,8 +7,6 @@ public final class Reels {
     private final RandomNumberGenerator randomNumberGenerator;
 
     private final List<Reel> reelList = new ArrayList<>();
-//    private final List<List<String>> rawReels;
-//    private Screen screen;
 
     public Reels(List<List<String>> rawReels, RandomNumberGenerator randomNumberGenerator) {
         this.randomNumberGenerator = randomNumberGenerator;
@@ -19,19 +17,6 @@ public final class Reels {
         }
 
 
-//        this.rawReels = rawReels;
-//        List<List<String>> rawScreen = rawReels.stream().map(
-//                reel -> {
-//                    int nextPosition = 0;
-//
-//                    return Stream.concat(reel.stream(), reel.stream()).toList().subList(
-//                            nextPosition, nextPosition + 3
-//                    );
-//                }
-//        ).toList();
-
-//        this.screen = new Screen(rawScreen);
-
     }
 
     public void spin() {
@@ -40,18 +25,6 @@ public final class Reels {
             reel.roll(randomNumberGenerator);
         }
 
-//        List<List<String>> rawScreen = rawReels.stream().map(
-//                reel -> {
-//
-//                    int nextPosition = randomNumberGenerator.nextInt(reel.size()); // command
-//
-//                    return Stream.concat(reel.stream(), reel.stream()).toList().subList( // prepare for query
-//                            nextPosition, nextPosition + 3
-//                    );
-//                }
-//        ).toList();
-
-//        this.screen = new Screen(rawScreen); // prepare for query
 
     }
 
