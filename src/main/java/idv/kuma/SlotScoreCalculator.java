9e6062e15b8787b988295230927feb4d23b47260
Yu-Song Syu <kuma@cloud-latitude.com>
2023-09-16 00:23:46 +0800

    extract methods

--- src/main/java/idv/kuma/SlotScoreCalculator.java
+++ src/main/java/idv/kuma/SlotScoreCalculator.java
@@ -42,6 +42,15 @@ public class SlotScoreCalculator {
 
         Screen screen = freeGameReels.getScreen();
 
+        int odd = getOddFreeGame(screen);
+
+        int win = odd * 10;
+
+        return new SpinResult(win, screen);
+
+    }
+
+    private int getOddFreeGame(Screen screen) {
         int odd = 0;
 
         int lines = screen.countStraightLines();
@@ -54,10 +63,6 @@ public class SlotScoreCalculator {
             odd = 100;
 
         }
-
-        int win = odd * 10;
-
-        return new SpinResult(win, screen);
-
+        return odd;
     }
 }
