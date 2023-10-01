57027cbc97eb996d6d902972a9dc42edd27dd000
Yu-Song Syu <kuma@cloud-latitude.com>
2023-10-01 22:43:49 +0800

    200 and 400

--- src/test/java/idv/kuma/DafuAllStarsPayTable.java
+++ src/test/java/idv/kuma/DafuAllStarsPayTable.java
@@ -1,8 +1,29 @@
 package idv.kuma;
 
+import java.util.List;
+
 public class DafuAllStarsPayTable implements PayTable {
     @Override
     public int getOdd(Screen screen) {
-        return 400;
+
+        List<List<String>> rawScreen = screen.rawScreen();
+
+        int count = 0;
+        for (List<String> row : rawScreen) {
+
+            if (row.get(0).equals("A")) {
+                count++;
+            } else {
+                break;
+            }
+
+        }
+
+        if (count == 5) {
+            return 400;
+        }
+
+        return 200;
+
     }
 }
--- src/test/java/idv/kuma/DafuAllStarsPayTableTest.java
+++ src/test/java/idv/kuma/DafuAllStarsPayTableTest.java
@@ -7,6 +7,27 @@ import java.util.List;
 
 class DafuAllStarsPayTableTest {
 
+
+    @Test
+    void A_4_200() {
+
+        PayTable sut = new DafuAllStarsPayTable();
+
+        Screen screen = new Screen(
+                List.of(
+                        List.of("A", "2", "3"),
+                        List.of("A", "4", "3"),
+                        List.of("A", "5", "3"),
+                        List.of("A", "4", "3"),
+                        List.of("2", "8", "4")
+                )
+        );
+        int actual = sut.getOdd(screen);
+
+        Assertions.assertThat(actual).isEqualTo(200);
+
+
+    }
     @Test
     void A_5_400() {
 
