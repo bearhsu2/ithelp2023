c809fc988ba76b7816a9804b47f5153303031c05
Yu-Song Syu <kuma@cloud-latitude.com>
2023-10-05 00:14:07 +0800

    re-order

--- src/test/java/idv/kuma/SlotScoreCalculatorSimulatorTest.java
+++ src/test/java/idv/kuma/SlotScoreCalculatorSimulatorTest.java
@@ -22,14 +22,14 @@ class SlotScoreCalculatorSimulatorTest {
                                 List.of("A", "A", "3", "A"),
                                 List.of("A", "A", "3", "A"),
                                 List.of("A", "A", "3"),
-                                List.of("A", "A", "3"),
-                                List.of("A", "A", "4")
+                                List.of("A", "A", "3", "A", "A"),
+                                List.of("A", "A", "4", "A", "A")
                         ), randomNumberGenerator), new SlotKingPayTable()),
                 new GameFlow(new Reels(
                         List.of(
                                 List.of("A", "2", "3", "3", "3"),
-                                List.of("A", "2", "3"),
-                                List.of("A", "3", "4")
+                                List.of("A", "2", "3", "A", "A", "A", "A"),
+                                List.of("A", "3", "4", "A", "A", "A", "A")
                         ), randomNumberGenerator
                 ), new MasterpiecePayTable())
         );
