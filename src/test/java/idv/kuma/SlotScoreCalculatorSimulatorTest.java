db44a3e58add9a92ffe1a28469f9efe528944714
Yu-Song Syu <kuma@cloud-latitude.com>
2023-10-10 22:03:19 +0800

    factory

--- src/test/java/idv/kuma/SlotScoreCalculatorSimulatorTest.java
+++ src/test/java/idv/kuma/SlotScoreCalculatorSimulatorTest.java
@@ -4,35 +4,16 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.Random;
-
 class SlotScoreCalculatorSimulatorTest {
+
+    private final SlotCalculatorFactory slotCalculatorFactory = new SlotCalculatorFactory();
 
     @Disabled
     @Test
     void simulate() throws WrongModeException {
 
 
-        RandomNumberGenerator randomNumberGenerator = new NativeRandomNumberGenerator(new Random());
-
-        SlotScoreCalculator sut = new SlotScoreCalculator(
-                new GameFlow(new Reels(
-                        List.of(
-                                List.of("A", "A", "3", "A"),
-                                List.of("A", "A", "3", "A"),
-                                List.of("A", "A", "3"),
-                                List.of("A", "A", "3", "A", "A"),
-                                List.of("A", "A", "4", "A", "A")
-                        ), randomNumberGenerator), new SlotKingPayTable()),
-                new GameFlow(new Reels(
-                        List.of(
-                                List.of("A", "2", "3", "3", "3"),
-                                List.of("A", "2", "3", "A", "A", "A", "A"),
-                                List.of("A", "3", "4", "A", "A", "A", "A")
-                        ), randomNumberGenerator
-                ), new MasterpiecePayTable()), new GongXiFaCaiTriggerringRules()
-        );
+        SlotScoreCalculator sut = slotCalculatorFactory.create( "00001");
 
         int totalWin = 0;
         int totalBet = 0;
@@ -54,4 +35,5 @@ class SlotScoreCalculatorSimulatorTest {
         Assertions.assertThat(Math.abs(rtp - 80) < 1).isTrue();
 
     }
+
 }
