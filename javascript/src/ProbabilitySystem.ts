74ad727e379dad6ce7f6c3ea9eb742511b9b7065
Yu-Song Syu <kuma@cloud-latitude.com>
2025-02-16 22:59:51 +0800

    create new class: Reels

--- javascript/src/ProbabilitySystem.ts
+++ javascript/src/ProbabilitySystem.ts
@@ -1,18 +1,29 @@
+class Reels {
+    reels: Array<Array<string>>;
+
+    constructor(reels: Array<Array<string>>) {
+        this.reels = reels;
+    }
+
+}
+
 export class ProbabilitySystem {
 
-    reels: Array<Array<string>> = [
-        ['A', 'Q', 'K'],
-        ['A', 'Q', 'K'],
-        ['A', 'Q', 'K'],
-        ['A', 'Q', 'K'],
-        ['A', '10', 'J'],
-    ];
+    reels: Reels = new Reels(
+        [
+            ['A', 'Q', 'K'],
+            ['A', 'Q', 'K'],
+            ['A', 'Q', 'K'],
+            ['A', 'Q', 'K'],
+            ['A', '10', 'J'],
+        ]
+    );
 
     spin(betLine: string): number {
 
         const firstElementsSet = new Set<string>();
-        for (let i = 0; i < this.reels.length; i++) {
-            const reel = this.reels[i];
+        for (let i = 0; i < this.reels.reels.length; i++) {
+            const reel = this.reels.reels[i];
             firstElementsSet.add(reel[0]);
         }
 
