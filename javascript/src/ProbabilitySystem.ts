a55f51984e3adb7c0fb1d062deb789af68af3f23
Yu-Song Syu <kuma@cloud-latitude.com>
2025-02-16 15:59:04 +0800

    now can check first elements equality

--- javascript/src/ProbabilitySystem.ts
+++ javascript/src/ProbabilitySystem.ts
@@ -1,6 +1,24 @@
 export class ProbabilitySystem {
+
+    reels: Array<Array<string>> = [
+        ['A', 'Q', 'K'],
+        ['A', 'Q', 'K'],
+        ['A', 'Q', 'K'],
+        ['A', 'Q', 'K'],
+        ['A', '10', 'J'],
+    ];
+
     spin(betLine: string): number {
-        return betLine === 'L1'
+        
+        const firstElementsSet = new Set<string>();
+        for (let i = 0; i < this.reels.length; i++) {
+            const reel = this.reels[i];
+            firstElementsSet.add(reel[0]);
+        }
+
+        console.log(firstElementsSet);
+
+        return firstElementsSet.size === 1 && betLine === 'L1'
             ? 20
             : 0;
     }
