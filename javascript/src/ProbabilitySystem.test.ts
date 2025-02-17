f4cf40cbcd3d116a92bc9de0b1602dc4ce6182c7
Yu-Song Syu <kuma@cloud-latitude.com>
2025-02-17 23:19:29 +0800

    refactor: rename tests for better understanding

--- javascript/src/ProbabilitySystem.test.ts
+++ javascript/src/ProbabilitySystem.test.ts
@@ -2,11 +2,12 @@ import {ProbabilitySystem} from "./ProbabilitySystem";
 
 describe('probability system', () => {
 
-    test('lose', () => {
+    test('Row1 hit, bet L2 -> 0', () => {
         const sut = new ProbabilitySystem();
         expect(sut.spin('L2')).toBe(0);
     });
-    test('L1 hit -> 20', () => {
+
+    test('Row1 hit, bet L1 -> 20', () => {
         const sut = new ProbabilitySystem();
         expect(sut.spin('L1')).toBe(20);
     });
