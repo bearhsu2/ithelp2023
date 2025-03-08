26d7785723a5b76b6ebbff870842a8abfca8120b
Yu-Song Syu <kuma@cloud-latitude.com>
2025-03-09 00:49:50 +0800

    now can spin

--- javascript/src/Reels.ts
+++ javascript/src/Reels.ts
@@ -1,21 +1,32 @@
 export class Reels {
     reels: Array<Array<string>>;
+    private index: number;
+    private nextIndex: number;
 
-    private constructor(reels: Array<Array<string>>) {
+    private constructor(reels: Array<Array<string>>, nextIndex: number) {
         this.reels = reels;
+        this.index = 0;
+        this.nextIndex = nextIndex;
     }
 
 
-    isRowHit(row: any): boolean {
+    spin() {
+        this.index = this.nextIndex;
+    }
+
+    isRowHit(row: number): boolean {
+
+        const localRow: number = this.nextIndex + row;
+
         const uniqueElements = new Set<string>();
-        for (let i = 0; i < this.reels.length; i++) {
-            const reel = this.reels[i];
-            uniqueElements.add(reel[row]);
+        for (let i: number = 0; i < this.reels.length; i++) {
+            const reel: string[] = this.reels[i];
+            uniqueElements.add(reel[localRow]);
         }
         return uniqueElements.size === 1;
     }
 
-    static create(p0: number, rawReels: Array<Array<string>>): Reels {
-        return new Reels(rawReels);
+    static create(nextIndex: number, rawReels: Array<Array<string>>): Reels {
+        return new Reels(rawReels, nextIndex);
     }
 }
