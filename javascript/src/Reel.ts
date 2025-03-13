fae9adfc3bdf1bbb6f9ffa0e1f92b5ca0b6b5e95
Yu-Song Syu <kuma@cloud-latitude.com>
2025-03-13 14:07:00 +0800

    now Reel can spin by itself and handle it's own index

--- javascript/src/Reel.ts
+++ javascript/src/Reel.ts
@@ -1,15 +1,25 @@
+import {RandomNumberGenerator} from "./RandomNumberGenerator";
+
 export class Reel {
     symbols: Array<string>;
+    private randomNumberGenerator: RandomNumberGenerator;
+    private index: number;
 
-    constructor(symbols: Array<string>) {
+    constructor(symbols: Array<string>, randomNumberGenerator: RandomNumberGenerator) {
         this.symbols = symbols;
+        this.randomNumberGenerator = randomNumberGenerator;
+        this.index = 0;
     }
 
-    getScreenColumn(index: number): string[] {
-        return this.symbols.slice(index, index + 3);
+    getScreenColumn(): string[] {
+        return this.symbols.slice(this.index, this.index + 3);
     }
 
-    static from(reel: Array<string>) {
-        return new Reel(reel);
+    static from(reel: Array<string>, randomNumberGenerator: RandomNumberGenerator) {
+        return new Reel(reel, randomNumberGenerator);
+    }
+
+    spin() {
+        this.index = this.randomNumberGenerator.nextInteger();
     }
 }
--- javascript/src/Reels.ts
+++ javascript/src/Reels.ts
@@ -5,19 +5,18 @@ import {RandomNumberGenerator} from "./RandomNumberGenerator";
 export class Reels {
     reels: Array<Reel>;
     private indices: number[];
-    private randomNumberGenerator: RandomNumberGenerator;
 
     private constructor(reels: Array<Array<string>>, randomNumberGenerator: RandomNumberGenerator) {
-        this.reels = reels.map((reel: Array<string>): Reel => Reel.from(reel));
-        this.randomNumberGenerator = randomNumberGenerator;
+
+        this.reels = reels.map((reel: Array<string>): Reel => Reel.from(reel, randomNumberGenerator));
+
         this.indices = [0, 0, 0, 0, 0];
     }
 
 
     spin(): void {
-
         for (let i: number = 0; i < this.indices.length; ++i) {
-            this.indices[i] = this.randomNumberGenerator.nextInteger();
+            this.reels[i].spin();
         }
     }
 
@@ -25,11 +24,10 @@ export class Reels {
         return this.getScreen().isScreenRowHit(row);
     }
 
-
     getScreen(): Screen {
         const rawScreen: Array<Array<string>> = [];
         for (let i: number = 0; i < this.reels.length; i++) {
-            rawScreen.push(this.reels[i].getScreenColumn(this.indices[i]));
+            rawScreen.push(this.reels[i].getScreenColumn());
         }
         return Screen.from(rawScreen);
     }
