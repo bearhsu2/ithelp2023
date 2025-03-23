8cae6d90547443e47bd693e09df3c2b9e183525c
Yu-Song Syu <kuma@cloud-latitude.com>
2025-03-23 20:20:11 +0800

    multiple hits

--- javascript/src/PayTable.ts
+++ javascript/src/PayTable.ts
@@ -1,4 +1,3 @@
-import {Reels} from "./Reels";
 import {Bet} from "./Bet";
 import {Screen} from "./Screen";
 
@@ -6,15 +5,18 @@ export class PayTable {
 
     getOdd(screen: Screen, bet: Bet): number {
 
+        let odd = 0;
+
         if (screen.isScreenRowHit(0) && bet.includes('L1')) {
-            return 20;
+            odd += 20;
         }
         if (screen.isScreenRowHit(1) && bet.includes('L2')) {
-            return 20;
+            odd += 20;
         }
         if (screen.isScreenRowHit(2) && bet.includes('L3')) {
-            return 20;
+            odd += 20;
         }
-        return 0;
+
+        return odd;
     }
 }
