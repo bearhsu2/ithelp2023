179cabf9f8fc856e33c710ab6a479ecc22936c56
Yu-Song Syu <kuma@cloud-latitude.com>
2025-03-25 17:13:19 +0800

    formally use array as parameter for isHit

--- javascript/src/Screen.ts
+++ javascript/src/Screen.ts
@@ -12,7 +12,7 @@ export class Screen {
     }
 
     // 每一 Column 指定 Row 的 Symbol 都相同
-    isHit(...rows: number[]) {
+    isHit(rows: number[]) {
 
 
         if (rows.length != this.rawScreen.length) {
