33b737832f4d78e362d9d6bf9088dd1ecc15a7ba
Yu-Song Syu <kuma@cloud-latitude.com>
2025-04-21 23:00:35 +0800

    now can determine game type in getScreen

--- javascript/src/ProbabilitySystem.ts
+++ javascript/src/ProbabilitySystem.ts
@@ -9,6 +9,7 @@ export class ProbabilitySystem {
     private reels: Reels;
     private payTable: PayTable;
     private freeGameReels: Reels;
+    private nextGameType: string = "BASE_GAME";
 
     private constructor(reels: Reels, payTable: PayTable, freeGameReels: Reels) {
         this.reels = reels;
@@ -20,13 +21,18 @@ export class ProbabilitySystem {
     spin(bet: Bet): SpinResult {
         this.reels.spin();
         const screen: Screen = this.reels.getScreen();
-        let count = screen.countSymbol('S');
-        return SpinResult.of(this.payTable.getOdd(screen, bet), screen.getRawScreenClone(), count >= 3 ? "FREE_GAME" : "BASE_GAME");
+
+        this.nextGameType = screen.countSymbol('S') >= 3 ? "FREE_GAME" : "BASE_GAME";
+
+        return SpinResult.of(this.payTable.getOdd(screen, bet), screen.getRawScreenClone(), this.nextGameType);
+
     }
 
 
     getScreen() {
-        return this.freeGameReels.getScreen();
+        return this.nextGameType === "BASE_GAME"
+            ? this.reels.getScreen()
+            : this.freeGameReels.getScreen();
     }
 
     static create(reels: Reels, payTable: PayTable, freeGameReels: Reels):
