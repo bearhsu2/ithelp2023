0d264271035f972a399fbef0f02c3a018ea0f43d
Yu-Song Syu <kuma@cloud-latitude.com>
2025-05-30 13:33:49 +0800

    add return void type

--- javascript/src/DBCTool.ts
+++ javascript/src/DBCTool.ts
@@ -1,12 +1,12 @@
 export class DBCTool {
 
-    static require(checkCondition: () => boolean, message: string) {
+    static require(checkCondition: () => boolean, message: string): void {
         if (!checkCondition()) {
             throw new Error(message);
         }
     }
 
-    static ensure(checkCondition: () => boolean, message: string) {
+    static ensure(checkCondition: () => boolean, message: string): void {
         if (!checkCondition()) {
             throw new Error(message);
         }
